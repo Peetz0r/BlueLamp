bfa3f24a482adea660ee9479ef1176d9084db85e
Peetz0r <peter@haas-en-berg.nl>
2014-11-25 03:12:14 +0100

    Made the app even better and even more user-friendly

--- android/app/src/main/java/nl/haas_en_berg/bluelamp/BlueLampActivity.java
+++ android/app/src/main/java/nl/haas_en_berg/bluelamp/BlueLampActivity.java
@@ -1,9 +1,12 @@
 package nl.haas_en_berg.bluelamp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -19,6 +22,7 @@ import android.widget.Button;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.larswerkman.holocolorpicker.ColorPicker;
@@ -62,6 +66,8 @@ public class BlueLampActivity extends Activity {
         settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
 
         toggle = (ToggleButton) findViewById(R.id.toggle);
+        toggle.setTextOn(getResources().getText(R.string.connected));
+        toggle.setTextOff(getResources().getText(R.string.not_connected));
 
         picker = (ColorPicker) findViewById(R.id.picker);
         picker.addSaturationBar((SaturationBar) findViewById(R.id.saturationbar));
@@ -75,7 +81,8 @@ public class BlueLampActivity extends Activity {
             if (Arrays.equals(cert_sha1, peetz0r_sha1)) {
                 bottom_text.setText("Gecompiled door Peter Hazenberg");
             } else {
-                bottom_text.setVisibility(View.GONE);
+                // Als je je eigen naam in beeld wilt hebben, gebruik dan de volgende regel:
+                //bottom_text.setText("Gecompiled door JOUW NAAM");
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -113,6 +120,7 @@ public class BlueLampActivity extends Activity {
         HashMap<String, String> map = (HashMap<String, String>) spinner.getSelectedItem();
         if (map != null) {
             toggle.setEnabled(false);
+            toggle.setText(R.string.connecting);
             address = map.get("address");
             Log.i(TAG, "Connecting to " + address);
             new BTConnectTask().execute();
@@ -190,10 +198,93 @@ public class BlueLampActivity extends Activity {
 
     @Override
     protected void onResume() {
-        populateSpinner();
-        toggle.performClick();
-
         super.onResume();
+
+        spinner = (Spinner) findViewById(R.id.spinner1);
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (btAdapter == null) {
+            Log.e(TAG, "Bluetooth not supported, telling user...");
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.unsupported_dialog_title).setMessage(R.string.unsupported_dialog_text);
+            builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Log.i(TAG, "Exiting");
+                    finish();
+                }
+            });
+            builder.create().show();
+            return;
+        } else {
+            if (btAdapter.isEnabled()) {
+                Log.i(TAG, "Bluetooth ON");
+            } else {
+                Log.i(TAG, "Bluetooth OFF");
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                return;
+            }
+        }
+
+        String address = settings.getString("address", "");
+        int position = 0;
+
+        ArrayList<Map<String, String>> spinnerArray = new ArrayList<Map<String, String>>();
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                if (device.getBluetoothClass().getDeviceClass() == 0x1f00) {
+                    Map<String, String> data = new HashMap<String, String>(2);
+                    data.put("name", device.getName());
+                    data.put("address", device.getAddress());
+                    Log.i(TAG, device.getName() + "\t" + device.getAddress() + "\t" + String.valueOf(device.getBluetoothClass().getDeviceClass()));
+                    if (device.getAddress().equals(address)) {
+                        position = spinnerArray.size();
+                    }
+                    spinnerArray.add(data);
+                }
+            }
+        }
+
+        Log.i(TAG, "SpinnerArray size: "+String.valueOf(spinnerArray.size()));
+
+        if (spinnerArray.size() > 0) {
+            SimpleAdapter adapter = new SimpleAdapter(this, spinnerArray, android.R.layout.simple_list_item_2, new String[]{"name", "address"}, new int[]{android.R.id.text1, android.R.id.text2});
+            spinner.setAdapter(adapter);
+            spinner.setSelection(position);
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                    closeBtConnection();
+                    openBtConnection();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+
+                }
+            });
+        } else {
+            Log.i(TAG, "No Bluetooth devices found, telling user...");
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Log.i(TAG, "Going to Bluetooth settings");
+                    startActivity(new Intent(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS));
+                    dialog.dismiss();
+                    onWindowFocusChanged(hasWindowFocus());
+                }
+            });
+            builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Log.i(TAG, "Exiting");
+                    finish();
+                }
+            });
+            builder.setTitle(R.string.zero_devices_dialog_title).setMessage(R.string.zero_devices_dialog_text);
+            builder.create().show();
+        }
     }
 
     @Override
@@ -222,7 +313,7 @@ public class BlueLampActivity extends Activity {
                                 Log.d(TAG, "Got three bytes. Red: " + String.valueOf(color[0] & 0xff) + "\tGreen: " + String.valueOf(color[1] & 0xff) + "\tBlue: " + String.valueOf(color[2] & 0xff));
                                 result = Color.rgb(color[0] & 0xff, color[1] & 0xff, color[2] & 0xff);
                             } else {
-                                Log.d(TAG, "could not read exactly 3 bytes during checkColor.");
+                                Log.d(TAG, "Could not read exactly 3 bytes during checkColor.");
                             }
                             return true;
                         } catch (IOException e) {
@@ -265,58 +356,5 @@ public class BlueLampActivity extends Activity {
         }
 
         super.onPause();
-    }
-
-    private void populateSpinner() {
-        spinner = (Spinner) findViewById(R.id.spinner1);
-
-        btAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        if (btAdapter == null) {
-            Log.e(TAG, "Bluetooth not supported");
-            Log.i(TAG, "exiting");
-            return;
-        } else {
-            if (btAdapter.isEnabled()) {
-                Log.i(TAG, "Bluetooth ON");
-            } else {
-                Log.i(TAG, "Bluetooth OFF");
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-                return;
-            }
-        }
-
-        String address = settings.getString("address", "");
-        int position = 0;
-
-        ArrayList<Map<String, String>> spinnerArray = new ArrayList<Map<String, String>>();
-        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
-        if (pairedDevices.size() > 0) {
-            for (BluetoothDevice device : pairedDevices) {
-                Map<String, String> data = new HashMap<String, String>(2);
-                data.put("name", device.getName());
-                data.put("address", device.getAddress());
-                if (device.getAddress().equals(address)) {
-                    position = spinnerArray.size();
-                }
-                spinnerArray.add(data);
-            }
-        }
-        SimpleAdapter adapter = new SimpleAdapter(this, spinnerArray, android.R.layout.simple_list_item_2, new String[]{"name", "address"}, new int[]{android.R.id.text1, android.R.id.text2});
-        spinner.setAdapter(adapter);
-        spinner.setSelection(position);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                closeBtConnection();
-                openBtConnection();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
     }
 }
